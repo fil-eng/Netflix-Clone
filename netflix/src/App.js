94f641b96605414867920530dc6081c4d766d2b9
filmon <bare traverse>
2024-10-21 16:20:53 -0700

    intial commit

---
+++ netflix/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css"
+function App() {
+  return (
+    <div>
+      <h1 className="App">hello i started my project</h1>
+    </div>
+  );
+}
+
+export default App
