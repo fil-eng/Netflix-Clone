a19a06debe3ee6b9355b8d7f884cc5b7541eaf45
filmon <bare traverse>
2024-10-21 17:24:49 -0700

    other

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+import Home from "./pages/home/Home"
 function App() {
   return (
     <div>
-      <h1 className="App">hello i started my project</h1>
     </div>
   );
 }
 
-export default App
+export default App;
